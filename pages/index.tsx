1cb3ab07e6bc0587c582915b5c3d8ef374d4e941
Artem Chumak <artem.4x@gmail.com>
2023-07-25 12:14:32 +0700

    add first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Button, Htag } from '../components';
+
 export default function Home(): JSX.Element {
 
   return (
     <>
     <div>
-      <h1>Hello world!!!</h1>
+      <Htag tag='h1'>Hello world!</Htag>
+      <Button appearance='primary' arrow='right'>Click me</Button>
+      <Button appearance='ghost' arrow='down'>Click me</Button>
     </div>
     </>
   );
